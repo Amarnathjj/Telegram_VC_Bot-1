481ffcf475d7ca425be76cced8c4d402d80fc013
Amarnathjj <78036670+Amarnathjj@users.noreply.github.com>
2021-01-28 19:25:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-bot_token = "1432617791:AAECp1SXJe1ljhLSiCMkH_2poLACRsS9n2Q"
+bot_token = "1679506455:AAHuD7th4KNtEW3I45XdLObVrZ0tIBWFoQA"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
 sudo_chat_id = -1001196272721
 owner_id = 1221693726
